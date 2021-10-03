c8ccce9804bfe8dcdfa263dbfb9861409ecc1cec
GeFontecha <user.name>
2021-10-03 10:40:13 -0500

    0x01-variables_if_else_while/0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/* last digit great or less whit five*/
+int main(void)
+
+{
+	int n;
+	//int *last_digit;
+	int pc, k;
+	//int *ip;
+	
+	
+		srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	//pc = &n;
+	
+	//pc=c
+	
+	//last_digit = ("%.*s , 
+	/*scanf*/
+	if (n > 0)
+	{
+	/*printf("%d\n", n);*/
+	printf("%s is positive. %p \n", %c, &n);
+	}
+	else if (n == 0)
+	printf("%i is zero. \n", n);
+	if (n < 0)
+	printf("%i is negative. \n", n);
+	return (0);
+}
