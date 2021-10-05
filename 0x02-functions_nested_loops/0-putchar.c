e42a3503cde4cb35d05c897c2b3415cb4702f327
GeFontecha <user.name>
2021-10-05 18:36:08 -0500

    0x02-functions_nested_loops/

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - print world
+ *
+ * return: return(0)
+*/
+
+char cadena[] = "hola";
+
+int main(void)
+
+{
+	printf("%s",cadena);
+	/*printf("_putchar\n");*/
+	return(0);
+}
+
+
