7dfdfd90a80269f79726ca6e5f7e7667e6272268
GeFontecha <user.name>
2021-10-08 17:13:20 -0500

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,14 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0; 
+	i = 0;
 
-	while (i < 10) 
+	while (i < 10)
 	{
 	putchar(i); /*don't delete it!*/
 	}
 
 	printf("Infinite loop avoided! \\o//\n");
 
-	return;
+	return (0);
 }
