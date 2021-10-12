4d0333af3464d3ab0a0c8b0a39ca70dd9ca41d61
GeFontecha <user.name>
2021-10-11 22:48:56 -0500

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include "0-main.h"
 
+/**
+ * _puts - prints string array
+ *
+ * @str: cadena a imprimir
+ */
+
 void _puts(char *str)
 {
 	int s;
-	
+
 	s = 0;
-		
+
 	while (str[s] != '\0')
 	{
 		_putchar(str[s]);
